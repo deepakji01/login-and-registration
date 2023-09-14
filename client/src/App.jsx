717f902a51c7200355aefebe25b68908c7a9e105
deepakji01 <dg327294@gmail.com>
2023-09-14 14:45:04 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Signup from './signup'
+import{BrowserRouter,Routes,Route} from 'react-router-dom'
+import Login from './Login'
+
+function App() {
+  
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/register' element={<Signup/>}></Route>
+        <Route path='/login' element={<Signup/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  
+  )
